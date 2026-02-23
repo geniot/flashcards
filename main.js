479ff7f846284c9fbfc8d7a5975dde42de2c366e
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2026-02-23 16:33:44 +0100

    Finish the implementation

--- main.js
+++ main.js
@@ -13,8 +13,24 @@ $(document).ready(function () {
   $('#nextBtn').click(function (e) {
     nextWord(1);
   });
+  $('#updateBtn').click(function (e) {
+    updateLists();
+  });
+  $('#checkBtn').click(function (e) {
+    check();
+  });
   update(currentWordIndex);
+  $("#fromTextArea").text(fromArray.join("\n"));
+  $("#toTextArea").text(toArray.join("\n"));
 });
+
+function check(){
+  if ($('#checkInput').val()===toArray[currentWordIndex]){
+    $("#checkInput").css({"border-color": "green", "border-width":"10px", "border-style":"solid"});
+  }else{
+    $("#checkInput").css({"border-color": "red", "border-width":"10px", "border-style":"solid"});
+  }
+}
 
 function nextWord(inc) {
   currentWordIndex += inc;
@@ -24,6 +40,11 @@ function nextWord(inc) {
     currentWordIndex = 0;
   }
   update(currentWordIndex);
+}
+
+function updateLists(){
+  fromArray = $("#fromTextArea").val().split("\n");
+  toArray = $("#toTextArea").val().split("\n");
 }
 
 function update(index) {
